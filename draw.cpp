c910b02813d81ef92cd6d13f1d71136dbcab6757
duyputq <100561979+duyputq@users.noreply.github.com>
2022-05-19 00:19:15 +0700

    Update draw.cpp

--- draw.cpp
+++ draw.cpp
@@ -79,7 +79,7 @@ void renderGame(const string& guessedWord, const string& badGuesses) // hàm v�
 	if (badGuessCount > 0) {
 		cout << "You've made " << badGuessCount << " wrong ";
 		cout << (badGuessCount == 1 ? "guess" : "guesses");
-		cout << ": " << badGuesses << endl;
+		cout << ": "<< badGuesses << endl;
 	}
 }
 
@@ -140,7 +140,7 @@ const string& getNextHangMan() {
     "   |        \n"
     " -----      \n",
     "   ------------+     \n"
-    "   |      \\         \n"
+    "   |      \         \n"
     "   |      O          \n"
     "   |     /|\\        \n"
     "   |     / \\        \n"
@@ -159,16 +159,164 @@ const string& getNextHangMan() {
     return figure[(currentFigure++) % NUMBER_OF_FIGURES];
 }
 
-void displayFinalResult(bool won, const string& word) { // hàm in ra kết quả
-    while (true) {
-        system("cls");
-        //clearScreen();
-        if (won)
-            cout << "Congratulations! You win!" << endl;
-        else
-            cout << "You lost. The correct word is: "<< word
-            << endl;
-        cout << (won ? getNextDancingMan() : getNextHangMan());
-        this_thread::sleep_for(chrono::milliseconds(300));
-    }
+//void displayFinalResult(bool won, const string& word) { // hàm in ra kết quả
+//    while (true) {
+//        system("cls");
+//        //clearScreen();
+//        if (won)
+//            cout << "Congratulations! You win!" << endl;
+//        else
+//            cout << "You lost. The correct word is: "<< word
+//            << endl;
+//        cout << (won ? getNextDancingMan() : getNextHangMan());
+//        this_thread::sleep_for(chrono::milliseconds(300));
+//    }
+//}
+
+const string GAMEOVER[] ={
+" -------------        \n"
+" |           /        \n"
+" |          O         \n"
+" |        /|\\        \n"
+" |        / \\        \n"
+" |     \n"
+" ----- \n",
+" -------------        \n"
+" |           |        \n"
+" |           O        \n"
+" |          /|\\      \n"
+" |          / \\      \n"
+" |     \n"
+" ----- \n",
+" -------------        \n"
+" |            \\       \n"
+" |             O      \n"
+" |             /|\\   \n"
+" |             / \\   \n"
+" |     \n"
+" ----- \n",
+" -------------        \n"
+" |           |        \n"
+" |           O        \n"
+" |          /|\\      \n"
+" |          / \\      \n"
+" |     \n"
+" ----- \n",
+" -------------         \n"
+" |           /         \n"
+" |          O          \n"
+" |         /|\\        \n"
+" |         / \\        \n"
+" |     \n"
+" ----- \n",
+" -------------        \n"
+" |           |        \n"
+" |           O        \n"
+" |          /|\\      \n"
+" |          / \\      \n"
+" |     \n"
+" ----- \n",
+" -------------        \n"
+" |            \\       \n"
+" |             O      \n"
+" |            /|\\   \n"
+" |            / \\   \n"
+" |                    \n"
+" ----- \n",
+" -------------        \n"
+" |           |        \n"
+" |           O        \n"
+" |          /|\\      \n"
+" |          / \\      \n"
+" |     \n"
+" ----- \n",
+" -------------        \n"
+" |           /        \n"
+" |          O         \n"
+" |         /|\\        \n"
+" |         / \\        \n"
+" |     \n"
+" ----- \n",
+" -------------        \n"
+" |           |        \n"
+" |           O        \n"
+" |          /|\\      \n"
+" |          / \\      \n"
+" |     \n"
+" ----- \n",
+" -------------        \n"
+" |            \\       \n"
+" |             O      \n"
+" |            /|\\   \n"
+" |            / \\   \n"
+" |     \n"
+" ----- \n",
+" -------------        \n"
+" |           |        \n"
+" |           O        \n"
+" |          /|\\      \n"
+" |          / \\      \n"
+" |     \n"
+" ----- \n",
+};
+static string figure[] = {
+" O \n"
+" /|\\ \n"
+" | | \n",
+" O \n"
+" /|\\ \n"
+" / \\ \n",
+" \O //\n"
+"  | \n"
+" / \\ \n",
+" \\O/ \n"
+"  | \n"
+" / \\ \n",
+" O \n"
+" /|\ \n"
+" / \\ \n",
+"   O \n"
+" /|\\ \n"
+" / \\ \n" ,
+" O \n"
+" /|\\ \n"
+" / \\ \n" ,
+"   O \n"
+" /|\\ \n"
+" / \\ \n" ,
+" O \n"
+" /|\\ \n"
+" / \\ \n" ,
+"   O \n"
+" /|\\ \n"
+" / \\ \n" ,
+};
+
+void displayFinalResult(bool won, const string& word,int sum){
+if(won){
+        int i=0;
+    while(i<10){
+            system("cls");
+           // for (int i = 0; i < 30; i++) cout << endl;
+            cout << "Congratulations! You win! "<<"the word is "<<word<<endl;
+             cout << "Your Point : "<<sum<<"/7"<<endl;
+               cout<<figure[i];
+               i++;
+            this_thread::sleep_for(chrono::milliseconds(500));
+		}
+}
+
+	else{
+		int i=0;
+		while(i<12){
+                system("cls");
+          // for (int i = 0; i < 30; i++) cout << endl;
+        cout << "You lost. The correct word is: " << word<<endl;
+        cout << "Your Point : "<<sum<<"/7"<<endl;
+               cout<<GAMEOVER[i];
+
+               i++;
+            this_thread::sleep_for(chrono::milliseconds(500));
+		}
+	}
 }
