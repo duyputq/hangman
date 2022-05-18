423cbef3e86ef329668eb4bdc7f2f64ce1ca8c7e
duyputq <100561979+duyputq@users.noreply.github.com>
2022-05-19 00:19:32 +0700

    Update draw.h

--- draw.h
+++ draw.h
@@ -3,4 +3,4 @@
 using namespace std;
 
 void renderGame(const string& guessedWord, const string& badGuesses); // hàm vẽ giá treo cổ, và số lần đoán sai // vẽ hoạt ảnh
-void displayFinalResult(bool won, const string& word); // hàm in ra kết quả
+void displayFinalResult(bool won, const string& word, int sum); // hàm in ra kết quả
