078d1c5167058e3eef84ee1d8deee58b2eb7d4d4
duyputq <100561979+duyputq@users.noreply.github.com>
2022-05-18 19:55:37 +0700

    Add files via upload

---
+++ draw.h
@@ -0,0 +1,6 @@
+#include <iostream>
+
+using namespace std;
+
+void renderGame(const string& guessedWord, const string& badGuesses); // hàm vẽ giá treo cổ, và số lần đoán sai // vẽ hoạt ảnh
+void displayFinalResult(bool won, const string& word); // hàm in ra kết quả
